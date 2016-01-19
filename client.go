a6e34e9ad57f217015a7cc8f4b0ddab3b62808b8
Morten Krogh <morten.krogh@amberbio.com>
2016-01-19 15:42:01 +0100

    client

---
+++ client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
+	"os"
+)
+
+func client(wsUri string) {
+
+	origin := "http://localhost/"
+
+	config, err := websocket.NewConfig(wsUri, origin)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ws, err := websocket.DialConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The web socket is connected to %s\n", wsUri)
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("\nMessage: ")
+
+		line, err := inputReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = ws.Write([]byte(line))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		msg := make([]byte, 512)
+
+		n, err := ws.Read(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Reply: %s", msg[:n])
+	}
+}
--- go-websocket-cmd-line.go
+++ go-websocket-cmd-line.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"os"
-	"log"
 	"fmt"
-	"bufio"
-	"golang.org/x/net/websocket"
+	"os"
 )
 
 func main() {
@@ -14,47 +11,8 @@ func main() {
 		fmt.Print("usage: %s web-socket-uri\n", os.Args[0])
 		os.Exit(1)
 	}
-	
+
 	wsUri := os.Args[1]
 
-	// wsUri := "ws://echo.websocket.org/"
-	origin := "http://localhost/"
-
-	config, err := websocket.NewConfig(wsUri, origin)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ws, err := websocket.DialConfig(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("The web socket is connected to %s\n", wsUri)
-
-	inputReader := bufio.NewReader(os.Stdin)
-	
-	for {
-		fmt.Print("\nMessage: ")
-
-		line, err := inputReader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = ws.Write([]byte(line))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		msg := make([]byte, 512)
-	
-		n, err := ws.Read(msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-	
-		fmt.Printf("Reply: %s", msg[:n])
-	}
+	client(wsUri)
 }
