73ee6bd1b5d45d65705b5c2f8d3cd83d8e9cde72
Morten Krogh <morten.krogh@amberbio.com>
2016-01-23 14:08:51 +0100

    removed test line

--- writer.go
+++ writer.go
@@ -1,11 +1,9 @@
 package main
 
 func writer(info wsInfo) {
-
 	for {
 		select {
 		case wsMsg := <-info.messageChan:
-			info.conn.Write([]byte("OK"))
 			info.conn.Write(wsMsg.bytes)
 		case <-info.closeChan:
 			return
